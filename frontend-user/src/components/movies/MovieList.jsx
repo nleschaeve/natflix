7b8908942239cff136e546839daff9fb476e7431
LESCHAEVE--LEMAIRE Nathaniel <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-02-13 17:29:18 +0100

    Creation du composant MovieList

--- frontend-user/src/components/movies/MovieList.jsx
+++ frontend-user/src/components/movies/MovieList.jsx
@@ -1,10 +1,14 @@
-// Assemble plusieurs composants moléculaires
-function MovieList({ movies }) {
+import MovieCard from './MovieCard';
+function MovieList({ title, movies }) {
     return (
-        <div className="grid">
-            {movies.map(movie => (
-                <MovieCard key={movie.id} movie={movie} />
-            ))}
-        </div>
+        <section className="py-8">
+            <h2 className="text-2xl md:text-3xl font-bold mb-6 px-4">{title}</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 px-4">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </section>
     );
 }
+export default MovieList;
