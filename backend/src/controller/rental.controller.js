38c7e96cc6c9514864d06f4a555926915d8b4755
LESCHAEVE--LEMAIRE Nathaniel <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-03-25 09:56:59 +0100

    Création du fichier src/controllers/rental.controller.js

---
+++ backend/src/controller/rental.controller.js
@@ -0,0 +1,38 @@
+import Rental from "../models/Rental.js";
+import Movie from "../models/Movie.js";
+import Uesr from "../models/User.js";
+
+// @desc    Louer un film
+// @route   POST /api/rentals
+// @access  Private
+export const createRental = async (req, res, next) => {
+    console.log("fonction createRental");
+};
+
+// @desc    Obtenir les locations d'un utilisateur
+// @route   GET /api/rentals/my-rentals
+// @access  Private
+export const getMyRentals = async (req, res, next) => {
+    console.log("fonction getMyRentals");
+};
+
+// @desc    Obtenir toutes les locations (admin)
+// @route   GET /api/rentals
+// @access  Private
+export const getAllRentals = async (req, res, next) => {
+    console.log("fonction getAllRentals");
+};
+
+// @desc    Annuler une location
+// @route   DELETE /api/rentals/:id
+// @access  Private
+export const cancelRental = async (req, res, next) => {
+    console.log("fonction cancelRental");
+};
+
+// @desc    Obtenir les statistiques des locations
+// @route   GET /api/rentals/stats
+// @access  Private/admin
+export const getRentalStats = async (req, res, next) => {
+    console.log("fonction getRentalStats");
+};
